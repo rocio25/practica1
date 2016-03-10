1076fd5c8d886c65966213274e12a2347c52aa3f
ponce <rocio.chaves@alu.uhu.es>
2016-03-10 18:04:42 +0100

    version 2 bienvenidos

---
+++ practica1/src/main/java/mgcss/Main.java
@@ -0,0 +1,21 @@
+/*
+ Mostrar por pantalla los numeros del 0 al 10
+ */
+
+package mgcss;
+
+/**
+ *
+ * @author usuario
+ */
+public class Main {
+    
+    public static void main (String[] args){
+        System.out.println("Bienvenidos al horror");
+        
+        for (int i=0; i<10;i++){
+            System.out.println(i+",");
+    }
+    
+}
+}
